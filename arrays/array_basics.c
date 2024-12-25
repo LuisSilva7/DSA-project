4c539db2fb46c779b76e38d58f0afed168869ba8
LuisSilva7 <lmpsilva7@gmail.com>
2024-12-25 00:32:47 +0000

    add array_basics

---
+++ arrays/array_basics.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+void initialize_array(int *arr, int size) {
+    for (int i = 0; i < size; i++) {
+        arr[i] = i + 1;
+    }
+}
+
+void print_array(int *arr, int size) {
+    for (int i = 0; i < size; i++) {
+        printf("Element at index %d: %d\n", i, arr[i]);
+    }
+}
+
+void set_value_at_index(int *arr, int index, int value) {
+    if (index >= 0 && index < 5) {
+        arr[index] = value;
+    } else {
+        printf("Index out of bounds\n");
+    }
+}
+
+int main() {
+    int arr[5];
+    initialize_array(arr, 5);
+    print_array(arr, 5);
+
+    set_value_at_index(arr, 2, 100);
+    print_array(arr, 5);
+
+    return 0;
+}
